3243cdf360d71c83df01f101ee9fac05842e4684
rustamdaniyarov <52420729+rustamdaniyarov@users.noreply.github.com>
2019-07-06 09:17:44 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 module.exports = function warmup(temperature) {
-  if (temperature === -20) {
-    return -4;
-  }
-  if (temperature === 0) {
-    return 32;
-  }
-};
+  //var far = temperature*1.8 +32;
+	return temperature*1.8+32;
+  //if (temperature === -20) {
+  //  return -4;
+  //}
+  //if (temperature === 0) {
+  //  return 32;
+}
